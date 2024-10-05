83c4bb96567201a5763de0ada960a6ecfb4c506f
arya-anathema <183879173+arya-anathema@users.noreply.github.com>
2024-10-05 18:38:43 -0400

    Readme clarification

--- backend/src/cards.py
+++ backend/src/cards.py
@@ -18,10 +18,30 @@ class WashingMachine(BaseCard):
     def change_state(self, old: GameState, played_by: Player) -> GameState:
         new_host_state = old.host_state.model_copy(update={
             "throw": old.host_state.throw.cycle().cycle(),
+            # return self if (conditional==true) else none
             "played_card": self if (played_by == Player.HOST) else None
         })
         new_guest_state = old.guest_state.model_copy(update={
             "throw": old.guest_state.throw.cycle().cycle(),
+            "played_card": self if (played_by == Player.GUEST) else None
+        })
+        return old.model_copy(update={
+            "host_state": new_host_state,
+            "guest_state": new_guest_state
+        })
+
+class AusWashingMachine(BaseCard):
+    name: str = "Australian Washing Machine"
+    path: str = "auswashing.png"
+    description: str = "Cycles throws to what they lose to"
+
+    def change_state(self, old: GameState, played_by: Player) -> GameState:
+        new_host_state = old.host_state.model_copy(update={
+            "throw": old.host_state.throw.cycle(),
+            "played_card": self if (played_by == Player.HOST) else None
+        })
+        new_guest_state = old.guest_state.model_copy(update={
+            "throw": old.guest_state.throw.cycle(),
             "played_card": self if (played_by == Player.GUEST) else None
         })
 
@@ -34,5 +54,6 @@ class WashingMachine(BaseCard):
 def list_cards() -> list[BaseCard]:
     return [
         DoNothing(),
-        WashingMachine()
+        WashingMachine(),
+        AusWashingMachine()
     ]
