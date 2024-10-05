c16ef14fe24cc84f8d038c48aa7b23d350dc543d
Miranda-M <mirandamanning789@gmail.com>
2024-10-05 16:24:35 -0400

    mrrow (card image stuff)

--- frontend/src/components/card.tsx
+++ frontend/src/components/card.tsx
@@ -8,11 +8,34 @@ interface CardProps {
 export default function Card(props: CardProps) {
     const card = props.info
 
-    return <div style={{background: "black", width: "10rem", height: "20rem"}}>
-        {card.name}
-        <div style={{height: "8rem", background: "red"}}>
-            picture here
+    return (
+        <div style={{
+            background: "white",
+            color: "black",
+            width: "12rem",
+            height: "20rem",
+            border: "2px solid black",
+            borderRadius: "10px"
+        }}>
+            {card.name}
+            <div style={{ height: "8rem", background: "red", padding: "1rem"}}>
+                <div style={{
+                    background: "white",
+                    height: "100%"
+                }}>
+                    <img
+                        src={"./assets/card_sprite_1.png"}
+                        alt={"image error :("}
+                        style={{
+                            width: "100%",
+                            height: "100%",
+                            objectFit: "cover",
+                            borderRadius: "5px"
+                        }}
+                    />
+                </div>
+            </div>
+            {card.description}
         </div>
-        {card.description}
-    </div>
+    );
 }
