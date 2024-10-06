fcdebd6e8f73f3d05c6198948264499c4f1eeb9a
arya-anathema <183879173+arya-anathema@users.noreply.github.com>
2024-10-06 01:57:25 -0400

    (Nearly) finalized cards.py :3

--- backend/src/cards.py
+++ backend/src/cards.py
@@ -27,6 +27,30 @@ def shared_change_state(old: GameState, played_by: Player) -> [GameState, bool]:
 
     return old, True
 
+def angel_save(old: GameState, player_saved: Player) -> GameState:
+    throw_types = [Throw.SCISSORS,Throw.PAPER,Throw.ROCK]
+    rand_throw = randint(0,2)
+
+    if player_saved is Player.HOST:
+        old.host_state.status_effects.remove("angel")
+        throw = rand_throw if (old.guest_state.throw is Throw.NONE) else old.guest_state.throw.cycle().cycle()
+        new_host_state = old.host_state.model_copy(update={
+            "throw": throw,
+            "status_effect": old.host_state.status_effects
+        })
+        new_guest_state = old.guest_state.model_copy()
+    else:
+        old.guest_state.status_effects.remove("angel")
+        throw = rand_throw if (old.host_state.throw is Throw.NONE) else old.host_state.throw.cycle().cycle()
+        new_guest_state = old.guest_state.model_copy(update={
+            "throw": throw,
+            "status_effect": old.guest_state.status_effects
+        })
+        new_host_state = old.host_state.model_copy()
+    return old.model_copy(update={
+        "host_state": new_host_state,
+        "guest_state": new_guest_state
+    })
 
 class DoNothing(BaseCard):
     name: str = "Does Nothing"
@@ -91,7 +115,7 @@ class AusWashingMachine(BaseCard):
 
 class CardJail(BaseCard):
     name: str = "Card Jail"
-    path: str = "cardjail.png"
+    path: str = "card_jail.png"
     description: str = "Opponent's next card has no effect."
 
     def change_state(self, old: GameState, played_by: Player) -> GameState:
@@ -122,31 +146,31 @@ class CardJail(BaseCard):
             "guest_state": new_guest_state
         })
 
-# class OppositeDay(BaseCard):
-#     name: str = "Opposite Day"
-#     path: str = "oppositeday.png"
-#     description: str = "At the end of the game, if you would lose, win instead!"
-#
-#     def change_state(self, old: GameState, played_by: Player) -> GameState:
-#
-#         shared, should_continue = shared_change_state(old, played_by)
-#         if not should_continue:
-#             return shared
-#
-#         if played_by is "host":
-#             new_guest_state = old.guest_state.model_copy(update={
-#                 "status_effects": old.guest_state.status_effects.append("jail")
-#             })
-#
-#         else:
-#             new_host_state = old.host_state.model_copy(update={
-#                 "status_effects": old.host_state.status_effects.append("jail")
-#             })
-#
-#         return old.model_copy(update={
-#             "host_state": new_host_state,
-#             "guest_state": new_guest_state
-#         })
+class OppositeDay(BaseCard):
+    name: str = "Opposite Day"
+    path: str = "oppositeday.png"
+    description: str = "At the end of the game, if you would lose, win instead!"
+
+    def change_state(self, old: GameState, played_by: Player) -> GameState:
+        if played_by is Player.HOST:
+            old.host_state.status_effects.append("winflip")
+            new_host_state = old.host_state.model_copy(update={
+                "status_effects": old.host_state.status_effects,
+                "played_card": self
+            })
+            new_guest_state = old.guest_state.model_copy()
+        else:
+            old.guest_state.status_effects.append("winflip")
+            new_guest_state = old.guest_state.model_copy(update={
+                "status_effects": old.guest_state.status_effects,
+                "played_card": self
+            })
+            new_host_state = old.host_state.model_copy()
+
+        return old.model_copy(update={
+            "host_state": new_host_state,
+            "guest_state": new_guest_state
+        })
 
 class Angel(BaseCard):
     name: str = "Angel's Chorus"
@@ -197,12 +221,16 @@ class Cat(BaseCard):
         })
 
         if old.host_state.throw is Throw.PAPER:
+            if("angel" in old.host_state.status_effects):
+                return angel_save(old,Player.HOST)
             new_host_state = old.host_state.model_copy(update={
                 "throw": old.host_state.throw.NONE,
                 "played_card": self if (played_by == Player.HOST) else None
             })
 
         if old.guest_state.throw is Throw.PAPER:
+            if("angel" in old.guest_state.status_effects):
+                return angel_save(old,Player.GUEST)
             new_guest_state = old.guest_state.model_copy(update={
                 "throw": old.guest_state.throw.NONE,
                 "played_card": self if (played_by == Player.GUEST) else None
@@ -231,11 +259,15 @@ class Screw(BaseCard):
         })
 
         if played_by is Player.HOST and old.host_state.throw is Throw.SCISSORS:
+            if("angel" in old.host_state.status_effects):
+                return angel_save(old,Player.HOST)
             new_host_state = old.host_state.model_copy(update={
                 "throw": old.host_state.throw.NONE,
                 "played_card": self
             })
         if played_by is Player.GUEST and old.guest_state.throw is Throw.SCISSORS:
+            if("angel" in old.guest_state.status_effects):
+                return angel_save(old,Player.GUEST)
             new_guest_state = old.guest_state.model_copy(update={
                 "throw": old.guest_state.throw.NONE,
                 "played_card": self
@@ -264,10 +296,14 @@ class Gun(BaseCard):
         })
 
         if played_by is Player.HOST and old.guest_state.throw is Throw.ROCK:
+            if("angel" in old.guest_state.status_effects):
+                return angel_save(old,Player.GUEST)
             new_guest_state = old.guest_state.model_copy(update={
                 "throw": old.guest_state.throw.NONE,
             })
         if played_by is Player.GUEST and old.host_state.throw is Throw.ROCK:
+            if("angel" in old.host_state.status_effects):
+                return angel_save(old,Player.HOST)
             new_host_state = old.host_state.model_copy(update={
                 "throw": old.host_state.throw.NONE,
             })
@@ -279,7 +315,7 @@ class Gun(BaseCard):
 
 class DevilDice(BaseCard):
     name: str = "Deal with the Devil"
-    path: str = "evil.png"
+    path: str = "devildice.png"
     description: str = "Reroll yours and your opponent's throw, but watch out! The devil giveth and the devil taketh away. (Your throw may be destroyed)"
 
     def change_state(self, old: GameState, played_by: Player) -> GameState:
@@ -292,8 +328,10 @@ class DevilDice(BaseCard):
         player_rand = randint(0,3)
 
         if played_by is Player.HOST:
-            host_throw = throw_types[player_rand]
             guest_throw = throw_types[opponent_rand]
+            host_throw = throw_types[player_rand]
+            if("angel" in old.host_state.status_effects and host_throw is Throw.NONE):
+                return angel_save(old,Player.HOST)
             new_host_state = old.host_state.model_copy(update={
                 "throw": host_throw,
             })
@@ -303,6 +341,8 @@ class DevilDice(BaseCard):
         else:
             host_throw = throw_types[opponent_rand]
             guest_throw = throw_types[player_rand]
+            if("angel" in old.guest_state.status_effects and guest_throw is Throw.NONE):
+                return angel_save(old,Player.GUEST)
             new_host_state = old.host_state.model_copy(update={
                 "throw": host_throw,
             })
@@ -320,6 +360,7 @@ def list_cards() -> list[BaseCard]:
         WashingMachine(),
         AusWashingMachine(),
         CardJail(),
+        OppositeDay(),
         Angel(),
         Cat(),
         Screw(),
