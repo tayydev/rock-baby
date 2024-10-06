04c4862598180ea23a8d73a4644fd0782c4e4ac1
tayydev <51377975+tayydev@users.noreply.github.com>
2024-10-05 21:14:14 -0400

    New structure

---
+++ frontend/src/pages/game/game.tsx
@@ -0,0 +1,56 @@
+import {useEffect, useState} from "preact/hooks";
+import {Configuration, DefaultApi, LobbyState, LobbyStatus} from "../../client";
+import {route} from "preact-router";
+
+import './game.css'
+import Playing from "./playing.tsx";
+import Showdown from "./showdown.tsx";
+import Created from "./created.tsx";
+
+
+const apiConfig = new Configuration({
+    basePath: 'http://127.0.0.1:8000', // Your FastAPI base URL
+});
+
+const apiClient = new DefaultApi(apiConfig);
+
+export function Game() {
+
+    const params = new URLSearchParams(window.location.search);
+    const role = params.get('role') || route("/error");
+    const queryStringId = params.get('gameID');
+
+    const [lobby, setLobby] = useState<LobbyState | null>(null);
+
+    //constant updater thing
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            if(queryStringId == null) return;
+            apiClient.getGameGetGameGet(queryStringId!)
+                .then(game => setLobby(game.data))
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    useEffect(() => {
+        if (role === 'host' && lobby === null) {
+            apiClient.createGameCreateGameGet().then(result => {
+                setLobby(result.data);
+                route(`/game?role=${role}&gameID=${result.data.id}`, true)
+            });
+        }
+        if (role === 'guest') {
+            if(lobby === null) route("/error");
+            else {
+                //TODO: join game here
+            }
+        }
+    }, []);
+
+    return <>
+        {(lobby == null || lobby.status == LobbyStatus.Created) && <Created lobby={lobby} role={role!}/>}
+        {(lobby != null && lobby.status == LobbyStatus.Playing) && <Playing lobbyState={lobby}/>}
+        {(lobby != null && lobby.status == LobbyStatus.Showdown) && <Showdown lobbyState={lobby}/>}
+    </>
+}
---
+++ frontend/src/pages/game/playing.tsx
@@ -0,0 +1,5 @@
+export default function Playing(){
+    return <div>
+        <p>Playing</p>
+    </div>
+}
