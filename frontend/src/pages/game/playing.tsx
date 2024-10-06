c7c8f96781bc22feb7b9f5b9a81a8627c76d008e
Whitt <whittsanders@gmail.com>
2024-10-05 22:26:54 -0400

    RPS Buttons

--- frontend/src/pages/game/playing.tsx
+++ frontend/src/pages/game/playing.tsx
@@ -1,5 +1,15 @@
+import {Throw} from "../../client";
+import {useState} from "preact/hooks";
+
 export default function Playing(){
+    const [selectedRPS, setSelectedRPS] = useState<Throw | null>(null);
+
     return <div>
-        <p>Playing</p>
+        <div>
+            <button onClick={() => setSelectedRPS(Throw.Rock)}>Rock</button>
+            <button onClick={() => setSelectedRPS(Throw.Paper)}>Paper</button>
+            <button onClick={() => setSelectedRPS(Throw.Scissors)}>Scissors</button>
+        </div>
+        <p>{selectedRPS ? selectedRPS : "Choice"}</p>
     </div>
 }
